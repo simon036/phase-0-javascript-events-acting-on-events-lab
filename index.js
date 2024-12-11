125606be6649b759eb2e9aeb7578a019cdf00212
Simon <ndungusimon564@gmail.com>
2024-12-12 00:07:40 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Your code here
+    function moveDodgerLeft() {
+        const dodger = document.getElementById('dodger');
+        const leftPosition = parseInt(dodger.style.left);
+        if (leftPosition > 0) {
+            dodger.style.left = `${leftPosition - 10}px`;
+        }
+    }
+
+    function moveDodgerRight() {
+        const dodger = document.getElementById('dodger');
+        const leftPosition = parseInt(dodger.style.left);
+        if (leftPosition < 360) { 
+            dodger.style.left = `${leftPosition + 10}px`;
+        }
+    }
+
+    document.addEventListener('keydown', function(event) {
+        if (event.key === 'ArrowLeft') {
+            moveDodgerLeft();
+        } else if (event.key === 'ArrowRight') {
+            moveDodgerRight();
+        }
+    });
